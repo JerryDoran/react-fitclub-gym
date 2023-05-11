10f5c9b447dc9fa1b1956c635319a6c815f91e59
Jerome Doran <disneyfan9184@gmail.com>
2023-05-11 17:31:23 -0400

    fixed mobil navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,113 +7,113 @@ export default function Header() {
   const mobile = window.innerWidth <= 768 ? true : false;
 
   return (
-    <Flex justifyContent="space-between" alignItems="center">
+    <Flex justifyContent='space-between' alignItems='center'>
       <Image
-        src="https://github.com/ZainRk/FitClub-Starter/blob/master/src/assets/logo.png?raw=true"
-        alt="logo"
-        width="10rem"
-        height="3rem"
+        src='https://github.com/ZainRk/FitClub-Starter/blob/master/src/assets/logo.png?raw=true'
+        alt='logo'
+        width='10rem'
+        height='3rem'
       />
-      {menuOpen === false && mobile === true ? (
-        <Box
-          cursor="pointer"
-          backgroundColor="#3c3f45"
-          padding=".5rem"
-          borderRadius="5px"
-          onClick={() => setMenuOpen(true)}
-          position={{ base: 'fixed', md: 'static' }}
-          right={{ base: '10', md: '-200' }}
+
+      <Box
+        cursor='pointer'
+        backgroundColor='#3c3f45'
+        padding='.5rem'
+        borderRadius='5px'
+        onClick={() => setMenuOpen(true)}
+        position={{ base: 'fixed', md: 'static' }}
+        right={{ base: '10', md: '-200' }}
+        display={{ base: 'block', md: 'none' }}
+      >
+        <Image
+          src='https://github.com/ZainRk/FitClub-Starter/blob/master/src/assets/bars.png?raw=true'
+          alt='hamburger'
+          width='1.5rem'
+          height='1.25rem'
+        />
+      </Box>
+
+      <List
+        display={{ base: menuOpen ? 'flex' : 'none', md: 'flex' }}
+        gap='2rem'
+        color='white'
+        fontWeight='semibold'
+        fontSize='16px'
+        flexDirection={{ base: 'column', md: 'row' }}
+        backgroundColor={{ base: '#3c3f45', md: 'transparent' }}
+        padding={{ base: '2rem', md: '0' }}
+        zIndex={{ base: '99', md: '7' }}
+        position={{ base: 'fixed', md: 'static' }}
+        right={{ base: '2rem', md: 'auto' }}
+        marginTop={{ base: '20rem', md: 'auto' }}
+        borderRadius='7px'
+      >
+        <ListItem
+          cursor='pointer'
+          _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
         >
-          <Image
-            src="https://github.com/ZainRk/FitClub-Starter/blob/master/src/assets/bars.png?raw=true"
-            alt="hamburger"
-            width="1.5rem"
-            height="1.25rem"
-          />
-        </Box>
-      ) : (
-        <List
-          display="flex"
-          gap="2rem"
-          color="white"
-          fontWeight="semibold"
-          fontSize="16px"
-          flexDirection={{ base: 'column', md: 'row' }}
-          backgroundColor={{ base: '#3c3f45', md: 'transparent' }}
-          padding={{ base: '2rem', md: '0' }}
-          zIndex={{ base: '99', md: '7' }}
-          position={{ base: 'fixed', md: 'static' }}
-          right={{ base: '2rem', md: 'auto' }}
-          marginTop={{ base: '20rem', md: 'auto' }}
-          borderRadius="7px"
+          <Link
+            to='home'
+            span={true}
+            smooth={true}
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </Link>
+        </ListItem>
+        <ListItem
+          cursor='pointer'
+          _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
         >
-          <ListItem
-            cursor="pointer"
-            _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+          <Link
+            to='programs'
+            span={true}
+            smooth={true}
+            onClick={() => setMenuOpen(false)}
           >
-            <Link
-              to="home"
-              span={true}
-              smooth={true}
-              onClick={() => setMenuOpen(false)}
-            >
-              Home
-            </Link>
-          </ListItem>
-          <ListItem
-            cursor="pointer"
-            _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+            Programs
+          </Link>
+        </ListItem>
+        <ListItem
+          cursor='pointer'
+          _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+        >
+          <Link
+            to='reasons'
+            span={true}
+            smooth={true}
+            onClick={() => setMenuOpen(false)}
           >
-            <Link
-              to="programs"
-              span={true}
-              smooth={true}
-              onClick={() => setMenuOpen(false)}
-            >
-              Programs
-            </Link>
-          </ListItem>
-          <ListItem
-            cursor="pointer"
-            _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+            Why Us
+          </Link>
+        </ListItem>
+        <ListItem
+          cursor='pointer'
+          _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+        >
+          <Link
+            to='plans'
+            span={true}
+            smooth={true}
+            onClick={() => setMenuOpen(false)}
           >
-            <Link
-              to="reasons"
-              span={true}
-              smooth={true}
-              onClick={() => setMenuOpen(false)}
-            >
-              Why Us
-            </Link>
-          </ListItem>
-          <ListItem
-            cursor="pointer"
-            _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+            Plans
+          </Link>
+        </ListItem>
+        <ListItem
+          cursor='pointer'
+          _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
+        >
+          <Link
+            to='testimonials'
+            span={true}
+            smooth={true}
+            onClick={() => setMenuOpen(false)}
           >
-            <Link
-              to="plans"
-              span={true}
-              smooth={true}
-              onClick={() => setMenuOpen(false)}
-            >
-              Plans
-            </Link>
-          </ListItem>
-          <ListItem
-            cursor="pointer"
-            _hover={{ color: '#f48915', transition: 'color 200ms ease-in-out' }}
-          >
-            <Link
-              to="testimonials"
-              span={true}
-              smooth={true}
-              onClick={() => setMenuOpen(false)}
-            >
-              Testimonials
-            </Link>
-          </ListItem>
-        </List>
-      )}
+            Testimonials
+          </Link>
+        </ListItem>
+      </List>
     </Flex>
   );
 }
